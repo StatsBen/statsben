4e69e3d8342496333ac20c2630fdd23475112c22
Ben Clark <bclark@avalancheassociation.ca>
2019-04-19 14:53:02 -0700

    Added Firebase configuration to project

---
+++ src/authentication/firebase.js
@@ -0,0 +1,20 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+import { apiKey } from "./keys";
+
+let config = {
+  apiKey: apiKey,
+  authDomain: "adventures-v2.firebaseapp.com",
+  databaseURL: "https://adventures-v2.firebaseio.com",
+  projectId: "adventures-v2",
+  storageBucket: "adventures-v2.appspot.com",
+  messagingSenderId: "541274129388"
+};
+
+firebase.initializeApp(config);
+
+export const firestore = firebase.firestore();
+export const auth = firebase.auth();
+
+export default firebase;
