eae4134cda982084a8a033a63310492b5cf64205
Ben Clark <bclark@avalancheassociation.ca>
2019-04-28 10:53:34 -0700

    Implemented some entry validation and cleaning

--- src/utils.js
+++ src/utils.js
@@ -15,11 +15,54 @@ export const tidyEntry = entry => {
     let key = propertiesPair[0];
     let val = propertiesPair[1];
     if (val && val != "") {
-      tidiedEntry[key] = val;
+      if (val.seconds) {
+        console.log("CONVERTING DATE!!!");
+        tidiedEntry[key] = val.toDate().toString();
+      } else {
+        tidiedEntry[key] = val;
+      }
     } else {
       tidiedEntry[key] = null;
     }
   });
 
   return tidiedEntry;
+};
+
+export const validateEntry = entry => {
+  let errors = [];
+
+  // First, check for empty stuff...
+  if (!entry.hasOwnProperty("Name")) {
+    errors.push(new Error("Your entry HAS to have a name, idiot..."));
+  }
+
+  if (!entry.hasOwnProperty("Date")) {
+    errors.push(new Error("Date is required, dummy! Try again..."));
+  }
+
+  if (!entry.hasOwnProperty("Contents")) {
+    errors.push(new Error("an entry has to have contents!"));
+  }
+
+  Object.entries(entry).map(propertiesPair => {
+    let key = propertiesPair[0];
+    let val = propertiesPair[1];
+    if (val && val != "") {
+      // Make sure the date is valid...
+      if (key == "Date") {
+        try {
+          let cleanDate = new Date(entry["Date"]);
+          entry["Date"] = cleanDate;
+        } catch (error) {
+          errors.push(
+            new Error("the date you entered was invalid, try again!")
+          );
+          console.error(error.message);
+        }
+      }
+    }
+  });
+
+  return { errors, entry };
 };
