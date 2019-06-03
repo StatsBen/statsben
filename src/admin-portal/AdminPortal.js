d8f6a8201fb42bcba17b25fa470f5cb1a7dc88ab
Ben Clark <bclark@avalancheassociation.ca>
2019-06-03 14:47:46 -0700

    debugged some lil' UI issues in the Admin Portal

--- src/admin-portal/AdminPortal.js
+++ src/admin-portal/AdminPortal.js
@@ -3,6 +3,7 @@ import Login from "./Login";
 import Logout from "./Logout";
 import Editor from "./editor/Editor";
 import { auth, signInWithGoogle, signOut } from "../authentication/firebase";
+import "./admin-portal.css";
 
 class AdminPortal extends React.Component {
   constructor(props) {
@@ -32,13 +33,15 @@ class AdminPortal extends React.Component {
       <div id="admin-portal">
         <h3 id="admin-welcome-message">Welcome Back, Ben!</h3>
         {user ? (
-          <div>
+          <div id="admin-page">
             <Logout logout={this.logout} />
             <Editor />
+            <div style={{ float: "none", clear: "both", width: "100%" }} />
           </div>
         ) : (
           <Login login={this.login} />
         )}
+        <div style={{ float: "none", clear: "both", width: "100%" }} />
       </div>
     );
   }
