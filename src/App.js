ca5a4af539f6706d9595d6ac0ffcb336850ce7cd
Ben Clark <bclark@avalancheassociation.ca>
2019-07-20 12:42:16 -0700

    Implemented some code splitting to separate out the admin page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Router } from "@reach/router";
-import AdminPortal from "./admin-portal/AdminPortal";
-import EntriesView from "./entries-viewer/EntriesView";
+import Loadable from "react-loadable";
 import "./global-styles.css";
+
+const LoadableMainView = Loadable({
+  loader: () => import("./entries-viewer/EntriesView"),
+  loading() {
+    return <p>Loading ... ... ... </p>;
+  }
+});
+
+const LoadableAdminPortal = Loadable({
+  loader: () => import("./admin-portal/AdminPortal"),
+  loading() {
+    return <p>Admin&#146;ll be ready in a sec, Ben!</p>;
+  }
+});
 
 class App extends React.Component {
   render() {
     return (
       <div id="main">
         <Router>
-          <EntriesView path="/" />
-          <AdminPortal path="/admin" />
+          <LoadableMainView path="/" />
+          <LoadableAdminPortal path="/admin" />
         </Router>
       </div>
     );
