54980ae5942500d7d829696fda9fd0ab8d321996
Ben Clark <bclark@avalancheassociation.ca>
2019-07-31 09:30:20 -0700

    Moved long, hacky image captioning script out into its own file

--- src/entries-viewer/Entry.js
+++ src/entries-viewer/Entry.js
@@ -1,6 +1,6 @@
 import React from "react";
 import moment from "moment";
-import { globals } from "../globals";
+import { addCaptionToImgFromAltText } from "./long-functions/image-caption-script";
 // import { css } from "@emotion/core";
 
 class Entry extends React.Component {
@@ -9,49 +9,11 @@ class Entry extends React.Component {
     this.state = { entry: props.entry || null, hidden: true };
   }
 
-  /** This function crawls through all the child nodes of an Entry component
-   ** and finds all IMG elements, reads their "alt" attribute text, and
-   ** generates a caption for the photo from the text.  **/
-  addCaptionToImgFromAltText = entryElement => {
-    const imgs = entryElement.getElementsByTagName("img");
-
-    for (let i = 0; i < imgs.length; i++) {
-      const img = imgs[i];
-      const alt = img.getAttribute("alt");
-      const cls = img.getAttribute("class");
-      const src = img.getAttribute("src");
-
-      let newImgContainer = document.createElement("div");
-      newImgContainer.classList.add(cls);
-
-      let newImg = document.createElement("img");
-      newImg.setAttribute("alt", alt);
-      newImg.setAttribute("src", src);
-      newImg.setAttribute("style", "width: 100%;");
-
-      let captionStyle =
-        "font-family: " +
-        globals.fonts.accent +
-        "; font-size: 0.8em;" +
-        "color: " +
-        globals.colours.fadedDark +
-        ";";
-
-      let caption = document.createElement("span");
-      caption.innerHTML = alt;
-      caption.classList.add("caption");
-      caption.setAttribute("style", captionStyle);
-
-      newImgContainer.append(newImg);
-      newImgContainer.append(caption);
-
-      img.replaceWith(newImgContainer);
-    }
-  };
-
   componentDidMount() {
     this.addCaptionToImgFromAltText(this.element);
   }
+
+  addCaptionToImgFromAltText = addCaptionToImgFromAltText;
 
   render() {
     let { entry } = this.props;
---
+++ src/entries-viewer/long-functions/image-caption-script.js
@@ -0,0 +1,43 @@
+import { globals } from "../../globals";
+
+/** This function crawls through all the child nodes of an Entry component
+ ** and finds all IMG elements, reads their "alt" attribute text, and
+ ** generates a caption for the photo from the text.  **/
+const addCaptionToImgFromAltText = entryElement => {
+  const imgs = entryElement.getElementsByTagName("img");
+
+  for (let i = 0; i < imgs.length; i++) {
+    const img = imgs[i];
+    const alt = img.getAttribute("alt");
+    const cls = img.getAttribute("class");
+    const src = img.getAttribute("src");
+
+    let newImgContainer = document.createElement("div");
+    newImgContainer.classList.add(cls);
+
+    let newImg = document.createElement("img");
+    newImg.setAttribute("alt", alt);
+    newImg.setAttribute("src", src);
+    newImg.setAttribute("style", "width: 100%;");
+
+    let captionStyle =
+      "font-family: " +
+      globals.fonts.accent +
+      "; font-size: 0.8em;" +
+      "color: " +
+      globals.colours.fadedDark +
+      ";";
+
+    let caption = document.createElement("span");
+    caption.innerHTML = alt;
+    caption.classList.add("caption");
+    caption.setAttribute("style", captionStyle);
+
+    newImgContainer.append(newImg);
+    newImgContainer.append(caption);
+
+    img.replaceWith(newImgContainer);
+  }
+};
+
+export { addCaptionToImgFromAltText };
