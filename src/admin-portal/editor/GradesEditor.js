f173d613087800d0658c46ce440b459c5c9a30c1
Ben Clark <bclark@avalancheassociation.ca>
2019-07-31 10:49:41 -0700

    Stubbed new Grades Editor component

---
+++ src/admin-portal/editor/GradesEditor.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class GradesEditor extends React.Component() {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div>Poo</div>;
+  }
+}
+
+export default GradesEditor;
