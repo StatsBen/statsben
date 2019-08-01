b95ebf9ed028f03baaa870da875c79ce85aadfad
Ben Clark <bclark@avalancheassociation.ca>
2019-08-01 12:59:48 -0700

    stubbed a new form builder utility

---
+++ src/utils/form-builder.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const formBuilder = {
+  makeInputElement(attr, changeHandler) {
+    // STUB
+
+    return (
+      <div>
+        <label />
+        <input />
+      </div>
+    );
+  }
+};
+
+export { formBuilder };
