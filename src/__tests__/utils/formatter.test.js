d7064a416fad33667bb2cc9b06a3baa1014f5535
Ben Clark <bclark@avalancheassociation.ca>
2019-08-01 14:57:13 -0700

    Stubbed some tests for the forthcoming parser and formatter utils

---
+++ src/__tests__/utils/formatter.test.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+import { formatter } from "../../utils/formatter";
+
+test("Test Pretty Date", () => {});
+
+test("Test Pretty Scrambling Grade", () => {});
+
+test("Pretty Commitment Grade", () => {});
+
+test("Test Pretty Distance", () => {});
+
+test("Test Pretty Vert", () => {});
---
+++ src/__tests__/utils/parser.test.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+import { parser } from "../../utils/parser";
+
+test("Test Parse Commitment Grade", () => {});
+
+test("Test Parse Distance", () => {});
+
+test("Test Parse Ice Grade", () => {});
+
+test("Test Parse YDS", () => {});
+
+test("Test Parse Scrambling Grade", () => {});
+
+test("Test Parse Vert", () => {});
