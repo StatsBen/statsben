9f11e0ddf8724d774bb8d4a16dbd91769f9d299f
Ben Clark <bclark@avalancheassociation.ca>
2019-08-01 10:54:27 -0700

    Stubbed a new validator object that will have a bunch of methods for validating entries

---
+++ src/utils/validator.js
@@ -0,0 +1,14 @@
+import { globals } from "../globals";
+
+const validator = {
+  /* returns a boolean, true if valid, false if not */
+  validateEntryAttributeByName(attribute, name) {
+    let valid = true;
+
+    // STUB
+
+    return valid;
+  }
+};
+
+export { validator };
