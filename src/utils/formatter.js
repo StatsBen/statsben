e3f373adf87b5f260dfaddb29c08b087fffc4482
Ben Clark <bclark@avalancheassociation.ca>
2019-08-01 14:56:47 -0700

    Stubbed a formatter and a parser util

---
+++ src/utils/formatter.js
@@ -0,0 +1,28 @@
+const formatter = {
+  prettyDate(raw) {
+    //STUB
+    return "12 - 12 - 12";
+  },
+
+  prettyScramblingGrade(raw) {
+    //STUB
+    return "Class 3";
+  },
+
+  prettyCommitmentGrade(raw) {
+    //STUB
+    return "Grade IV";
+  },
+
+  prettyDistance(raw) {
+    // STUB
+    return "80km";
+  },
+
+  prettyVert(raw) {
+    // STUB
+    return "1,300m";
+  }
+};
+
+export { formatter };
---
+++ src/utils/parser.js
@@ -0,0 +1,43 @@
+const parser = {
+  parseStringDate(raw) {
+    //STUB
+    return "12/12/12";
+  },
+
+  parseUTCDate(raw) {
+    // STUB
+    return "fart"
+  }
+
+  parseCommitmentGrade(raw) {
+    // STUB
+    return 3;
+  },
+
+  parseDistance(raw) {
+    //STUB
+    return 50;
+  },
+
+  parseIceGrade(raw) {
+    //STUB
+    return "WI4";
+  },
+
+  parseYDS(raw) {
+    //STUB
+    return "fart";
+  },
+
+  parseScramblingGrade(raw) {
+    //STUB
+    return 3;
+  },
+
+  parseVert(raw) {
+    //STUB
+    return 1500;
+  }
+};
+
+export { parser };
