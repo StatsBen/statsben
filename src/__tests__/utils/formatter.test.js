f268e8d9037189189e74a911926833236495e3a0
Ben Clark <bclark@avalancheassociation.ca>
2019-08-02 15:15:22 -0700

    Finished writing test for the utils

--- src/__tests__/utils/formatter.test.js
+++ src/__tests__/utils/formatter.test.js
@@ -1,12 +1,36 @@
 /* eslint-disable */
 import { formatter } from "../../utils/formatter";
 
-test("Test Pretty Date", () => {});
+test("Test Pretty Date", () => {
+  let testDate = new Date("1/2/03");
+  expect(formatter.prettyDate(testDate)).toEqual("01 - 01 - 03");
+  expect(formatter.prettyDate(12)).toThrow();
+  expect(formatter.prettyDate("stringyboi")).toThrow();
+});
 
-test("Test Pretty Scrambling Grade", () => {});
+test("Test Pretty Scrambling Grade", () => {
+  expect(formatter.prettyScramblingGrade(3)).toEqual("class 3");
+  expect(formatter.prettyScramblingGrade("5")).toThrow();
+  expect(formatter.prettyScramblingGrade(0)).toThrow();
+});
 
-test("Pretty Commitment Grade", () => {});
+test("Pretty Commitment Grade", () => {
+  expect(formatter.prettyCommitmentGrade(2)).toEqual("grade II");
+  expect(formatter.prettyCommitmentGrade(4)).toEqual("grade IV");
+  expect(formatter.prettyCommitmentGrade(6)).toEqual("grade VI");
+  expect(formatter.prettyCommitmentGrade("VI")).toThrow();
+  expect(formatter.prettyCOmmitmentGrade(0)).toThrow();
+});
 
-test("Test Pretty Distance", () => {});
+test("Test Pretty Distance", () => {
+  expect(formatter.prettyDistance(50)).toEqual("50km");
+  expect(formatter.prettyDistance("50k")).toThrow();
+  expect(formatter.prettyDistance(0)).toThrow();
+});
 
-test("Test Pretty Vert", () => {});
+test("Test Pretty Vert", () => {
+  expect(formatter.prettyVert(50)).toEqual("50m");
+  expect(formatter.prettyVert(1300)).toEqual("1,300m");
+  expect(formatter.prettyVert("poo")).toThrow();
+  expect(formatter.prettyVert(0)).toThrow();
+});
--- src/__tests__/utils/parser.test.js
+++ src/__tests__/utils/parser.test.js
@@ -1,14 +1,67 @@
 /* eslint-disable */
 import { parser } from "../../utils/parser";
 
-test("Test Parse Commitment Grade", () => {});
+test("Test Parse Commitment Grade", () => {
+  expect(parser.parseCommitmentGrade("II")).toEqual(2);
+  expect(parser.parseCommitmentGrade(2)).toEqual(2);
+  expect(parser.parseCommitmentGrade("IV")).toEqual(4);
+  expect(parser.parseCommitmentGrade("VI")).toEqual(6);
+  expect(parser.parseCommitmentGrade("VII")).toThrow();
+  expect(parser.parseCommitmentGrade(0)).toThrow();
+  expect(parser.parseCommitmentGrade(8)).toThrow();
+});
 
-test("Test Parse Distance", () => {});
+test("Test Parse Distance", () => {
+  expect(parser.parseDistance(12)).toEqual(12);
+  expect(parser.parseDistance("12k")).toEqual(12);
+  expect(parser.parseDistance("  12km ")).toEqual(12);
+  expect(parser.parseDistance("1,000")).toEqual(1000);
+  expect(parser.parseDistance("1,300km")).toEqual(1300);
+  expect(parser.parseDistance("100mi")).toEqual(170);
+  expect(parser.parseDistance(0)).toThrow();
+  expect(parser.parseDistance("0")).toThrow();
+  expect(parser.parseDistance("poo")).toThrow();
+  expect(parser.parseDistance("100fuckyous")).toThrow();
+});
 
-test("Test Parse Ice Grade", () => {});
+test("Test Parse Ice Grade", () => {
+  expect(parser.parseIceGrade("WI3")).toEqual("WI3");
+  expect(parser.parseIceGrade("AI4  ")).toEqual("AI4");
+  expect(parser.parseIceGrade("WI9")).toThrow();
+  expect(parser.parseIceGrade("fuckyou")).toThrow();
+  expect(parser.parseIceGrade("AI0")).toThrow();
+});
 
-test("Test Parse YDS", () => {});
+test("Test Parse YDS", () => {
+  expect(parser.parseYDSGrade("5.4")).toEqual("5.4");
+  expect(parser.parseYDSGrade("5.9+")).toEqual("5.9+");
+  expect(parser.parseYDSGrade("  5.10c  ")).toEqual("5.10c");
+  expect(parser.parseYDSGrade("5.11e")).toThrow();
+  expect(parser.parseYDSGrade("11b")).toThrow();
+  expect(parser.parseYDSGrade(9)).toThrow();
+  expect(parser.parseYDSGrade(5.4)).toThrow();
+  expect(parser.parseYDSGrade("5.9b")).toThrow();
+  expect(parser.parseYDSGrade("5.16")).toThrow();
+});
 
-test("Test Parse Scrambling Grade", () => {});
+test("Test Parse Scrambling Grade", () => {
+  expect(parser.parseScramblingGrade(3)).toEqual(3);
+  expect(parser.parseScramblingGrade("3")).toEqual(3);
+  expect(parser.parseScramblingGrade("class 3")).toEqual(3);
+  expect(parser.parseScramblingGrade(6)).toThrow();
+  expect(parser.parseScramblingGrade(0)).toThrow();
+  expect(parser.parseScramblingGrade("0")).toThrow();
+  expect(parser.parseScramblingGrade("poopz")).toThrow();
+});
 
-test("Test Parse Vert", () => {});
+test("Test Parse Vert", () => {
+  expect(parser.parseVert(50)).toEqual(50);
+  expect(parser.parseVert("50")).toEqual(50);
+  expect(parser.parseVert("50m")).toEqual(50);
+  expect(parser.parseVert("1,300")).toEqual(1300);
+  expect(parser.parseVert("1,300m")).toEqual(1300);
+  expect(parser.parseVert("1,,300m")).toThrow();
+  expect(parser.parseVert("50mi")).toThrow();
+  expect(parser.parseVert(0)).toThrow();
+  expect(parser.parseVert("poopin yr face")).toThrow();
+});
