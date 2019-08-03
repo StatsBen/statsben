34f41e0c30eb1a14acb763eb6a59d43059dd8419
Ben Clark <bclark@avalancheassociation.ca>
2019-08-02 17:06:16 -0700

    Started working on some parser functions but sorta did a shitty job at it...

--- src/utils/parser.js
+++ src/utils/parser.js
@@ -1,17 +1,66 @@
 const parser = {
-  parseStringDate(raw) {
-    //STUB
-    return "12/12/12";
-  },
-
-  parseUTCDate(raw) {
-    // STUB
-    return "fart";
+  parseDate(raw) {
+    try {
+      return new Date(raw);
+    } catch (e) {
+      console.error(
+        "Error in parser.parseDate: couldnt make Date object from input"
+      );
+      throw e;
+    }
   },
 
   parseCommitmentGrade(raw) {
-    // STUB
-    return 3;
+    if (typeof raw == "number") {
+      if (raw < 1 || raw > 6) {
+        console.error(
+          "Error in parser.parseCommitmentGrade: input was too big or too small."
+        );
+        throw new Error("Couldnt parse commitment grade");
+      } else {
+        return raw;
+      }
+    } else if (typeof raw == "string") {
+      let num = raw.match(/\d+/g);
+      if (num[0] == "undefined") {
+        console.error(
+          "Error in parser.parseCommitmentGrade: the provided string had no number in it."
+        );
+        throw new Error("couldnt parse commitment grade: no number provided");
+      }
+      let parsedNum = null;
+      try {
+        parsedNum = parseInt(num);
+      } catch (e) {
+        console.error(
+          "Error in parseCommitmentGrade: extracted " +
+            num +
+            "from the provided string but couldnt make a grade from it."
+        );
+        throw e;
+      }
+
+      if (parsedNum < 1 || parsedNum > 6) {
+        console.error(
+          "Error in parser.parseCommitmentGrade: input was too big or too small."
+        );
+        throw new Error("Couldnt parse commitment grade");
+      } else if (parsedNum == null) {
+        console.error(
+          "Error in parseCommitmentGrade: extracted " +
+            num +
+            "from the provided string but couldnt make a grade from it."
+        );
+        throw new Error("couldnt parse commitment grade");
+      } else {
+        return parsedNum;
+      }
+    } else {
+      console.error(
+        "Error in parser.parseCommitmentGrade: Unrecognized input type"
+      );
+      throw new Error("Couldnt parse commitment grade");
+    }
   },
 
   parseDistance(raw) {
