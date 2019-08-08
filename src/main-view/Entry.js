c9ebcc93692be5bf4e0d91130c4c0f61bee700a5
Ben Clark <bclark@avalancheassociation.ca>
2019-08-07 17:36:16 -0700

    Re-implemented stylings in the Entry component with Emotion

--- src/main-view/Entry.js
+++ src/main-view/Entry.js
@@ -3,6 +3,7 @@ import React from "react";
 import { css, jsx } from "@emotion/core";
 import moment from "moment";
 import { addCaptionToImgFromAltText } from "../utils/image-caption-script";
+import { globals } from "../globals";
 
 class Entry extends React.Component {
   constructor(props) {
@@ -14,19 +15,80 @@ class Entry extends React.Component {
     addCaptionToImgFromAltText(this.element);
   }
 
-  containerCSS = css``;
+  containerCSS = css`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    float: left;
+    width: 90%;
+    min-width: 550px;
+    margin: 0px 0 50px 0;
+    padding: 0 5% 0 5%;
+    @media (min-width: 1400px) {
+      width: 95%;
+      margin: 0 0 100px 0;
+      break-inside: avoid;
+      break-after: avoid;
+      page-break-inside: avoid;
+      padding: 0 2.5% 0 2.5%;
+    }
+  `;
+
+  outerDateCSS = css`
+    order: 1;
+    @media (max-width: 1400px) {
+      order: 1;
+      flex: 0 0 auto;
+    }
+    @media (min-width: 1400px) {
+      flex: 0 0 22px;
+    }
+  `;
+
+  innerDateCSS = css`
+    display: block;
+    position: relative;
+    padding-left: 25px;
+    writing-mode: vertical-lr;
+    text-align: right;
+    font-family: ${globals.fonts.accent};
+    font-size: 14pt;
+    text-orientation: mixed;
+    transform: rotate(180deg);
+    @media (min-width: 1400px) {
+      width: 22px;
+      min-height: 130px;
+    }
+  `;
+
+  contentsContainerCSS = css`
+    order: 2;
+    flex: 0 1 auto;
+    padding-left: 5%;
+    min-height: 10em;
+    border-left: thin solid #bbbbbb;
+  `;
 
   render() {
     let { entry } = this.props;
 
     return (
-      <div className={`entry-container`} ref={r => (this.element = r)}>
-        <div className={`entry-date`}>
-          <span>{moment(entry.date.toDate()).format("MM - DD - YY")}</span>
+      <div css={this.containerCSS} ref={r => (this.element = r)}>
+        <div css={this.outerDateCSS}>
+          <span css={this.innerDateCSS}>
+            {moment(entry.date.toDate()).format("MM - DD - YY")}
+          </span>
         </div>
-        <div className={`entry-right`}>
+        <div css={this.contentsContainerCSS}>
           <div className={`entry-title`}>
-            <h3>{entry.name}</h3>
+            <h3
+              css={css`
+                padding: 0 0 30px 0;
+                margin: 0;
+              `}
+            >
+              {entry.name}
+            </h3>
           </div>
           <div
             className={`entry-contents`}
