641baf906c716257b60852c0f358d0fa7bc7f130
Ben Clark <bclark@avalancheassociation.ca>
2019-08-10 15:38:10 -0700

    Updated testing entry JSON to match latest definition

--- src/__tests__/demo-entries/demo-valid-entry.js
+++ src/__tests__/demo-entries/demo-valid-entry.js
@@ -2,11 +2,12 @@
 
 const validEntry = {
   name: "Banana Peel + Boomstick + The Ultimate Everything",
-  dateString: "09/25/17",
-  dateUTC: "2017-09-25T07:00:00.000Z",
+  date: "2017-09-25T07:00:00.000Z",
+  range: "Coast Range",
   html:
     "<div>On this climb, Federico Andrade Rivas and I linked together Bananna Peel, Boomstick Crack, and The Ultimate Everything; climbing 19 pitches, roughly 600m, up to 10b, all the way to the summit of the chief in Squamish over roughly 8 hours. An extremely fun day of climbing!<br />\n                </div>",
-  range: "Coast Range",
+  longForm: "",
+
   types: {
     alpine: false,
     rock: true,
@@ -22,6 +23,7 @@ const validEntry = {
     other: false
   },
   grade: {
+    alpine: "PD+",
     rock: "5.10b",
     ice: null,
     distance: null,
