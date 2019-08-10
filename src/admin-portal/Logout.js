cb2668d440a5accc2a8f3bfa610026d08b639f90
Ben Clark <bclark@avalancheassociation.ca>
2019-08-10 12:03:15 -0700

    Half-assed some styling on that annoying Logout button...

--- src/admin-portal/Logout.js
+++ src/admin-portal/Logout.js
@@ -1,5 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
+/** @jsx jsx */
+import { css, jsx } from "@emotion/core";
+import { globals } from "../globals";
 
 class Logout extends React.Component {
   constructor(props) {
@@ -13,7 +16,23 @@ class Logout extends React.Component {
   render() {
     return (
       <div id="login-pane">
-        <button onClick={this.props.logout} className={`editor-button`}>
+        <button
+          onClick={this.props.logout}
+          className={`editor-button`}
+          css={css`
+            float: right;
+            font-size: 1.1em;
+            padding: 5px
+            margin: 50px 0 0 80%;
+            border-radius: 5px;
+            user-select: none;
+            background: ${globals.colours.lightBlue};
+            &:hover {
+              cursor: pointer;
+              background: white;
+            }
+          `}
+        >
           Log Out
         </button>
       </div>
