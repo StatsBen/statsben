34da8e26de546ec47da624545df165a0e030a3b2
Ben Clark <bclark@avalancheassociation.ca>
2019-10-27 11:43:51 -0700

    Added favicon, yay!

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Router } from "@reach/router";
 import Loadable from "react-loadable";
+import Favicon from "react-favicon";
 import "./global-styles.css";
 
 const LoadableMainView = Loadable({
@@ -22,6 +23,11 @@ class App extends React.Component {
   render() {
     return (
       <div id="main">
+        <Favicon
+          url={
+            "https://firebasestorage.googleapis.com/v0/b/adventures-41a04.appspot.com/o/favicon.png?alt=media&token=9d153378-2ec6-4934-a912-e705ec420f02"
+          }
+        />
         <Router>
           <LoadableMainView path="/" />
           <LoadableAdminPortal path="/admin" />
