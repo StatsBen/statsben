62d5a9b5eec22e2e3fb3934bcf3c5fe6f730c7af
Ben Clark <bclark@avalancheassociation.ca>
2019-11-17 14:07:10 -0800

    Switched out available banner images - might have to pick better ones again...

--- src/globals/banner-images.js
+++ src/globals/banner-images.js
@@ -1,12 +1,14 @@
 const bannerImgs = {
+  abbot:
+    "https://firebasestorage.googleapis.com/v0/b/adventures-41a04.appspot.com/o/images%2Flabbot1.jpg?alt=media&token=42b75fc2-3677-4314-8a03-56963c018e16",
+  adams:
+    "https://firebasestorage.googleapis.com/v0/b/adventures-41a04.appspot.com/o/images%2Fadams3.JPG?alt=media&token=8a764fd6-f110-413c-9ae9-cd30b4360937",
   baker:
     "https://firebasestorage.googleapis.com/v0/b/adventures-41a04.appspot.com/o/images%2Fbaker2.JPG?alt=media&token=acc4e2fe-53dc-47bb-a38a-4dd33459f736",
-  crown:
-    "https://firebasestorage.googleapis.com/v0/b/adventures-41a04.appspot.com/o/images%2Fcrown.jpg?alt=media&token=ba53b0a7-1b0e-43b6-ba68-e8da8c48f069",
-  furnace:
-    "https://firebasestorage.googleapis.com/v0/b/adventures-41a04.appspot.com/o/images%2Ffieryfurnace.jpg?alt=media&token=fc6a5404-9d03-4fbf-93fa-476d5aa3cd11",
-  abbot:
-    "https://firebasestorage.googleapis.com/v0/b/adventures-41a04.appspot.com/o/images%2Flabbot1.jpg?alt=media&token=42b75fc2-3677-4314-8a03-56963c018e16"
+  hood:
+    "https://firebasestorage.googleapis.com/v0/b/adventures-41a04.appspot.com/o/images%2Fhood2.JPG?alt=media&token=c50458d0-fcdc-448a-a7ad-9bbd5c2d8448",
+  milleneum:
+    "https://firebasestorage.googleapis.com/v0/b/adventures-41a04.appspot.com/o/images%2Fice3.jpg?alt=media&token=61ec890e-5f11-4742-af12-d18eddea95e1"
 };
 
 export { bannerImgs };
