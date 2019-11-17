b06cd8c6265428e21188495c344b29994d5808db
Ben Clark <bclark@avalancheassociation.ca>
2019-11-17 12:10:00 -0800

    Added new global constants for banner images

---
+++ src/globals/banner-images.js
@@ -0,0 +1,12 @@
+const bannerImgs = {
+  baker:
+    "https://firebasestorage.googleapis.com/v0/b/adventures-41a04.appspot.com/o/images%2Fbaker2.JPG?alt=media&token=acc4e2fe-53dc-47bb-a38a-4dd33459f736",
+  crown:
+    "https://firebasestorage.googleapis.com/v0/b/adventures-41a04.appspot.com/o/images%2Fcrown.jpg?alt=media&token=ba53b0a7-1b0e-43b6-ba68-e8da8c48f069",
+  furnace:
+    "https://firebasestorage.googleapis.com/v0/b/adventures-41a04.appspot.com/o/images%2Ffieryfurnace.jpg?alt=media&token=fc6a5404-9d03-4fbf-93fa-476d5aa3cd11",
+  abbot:
+    "https://firebasestorage.googleapis.com/v0/b/adventures-41a04.appspot.com/o/images%2Flabbot1.jpg?alt=media&token=42b75fc2-3677-4314-8a03-56963c018e16"
+};
+
+export { bannerImgs };
--- src/globals/index.js
+++ src/globals/index.js
@@ -1,10 +1,12 @@
+import { bannerImgs } from "./banner-images";
 import { colours } from "./colours";
 import { entryDefinition } from "./entry-definition";
 import { fonts } from "./fonts";
 import { sizes } from "./sizes";
 import { types } from "./types";
 
-let globals = {
+const globals = {
+  bannerImgs,
   colours,
   entryDefinition,
   fonts,
