077f9e733bb614d94a921aa6dd22b9aa6e92fc08
Ben Clark <bclark@avalancheassociation.ca>
2020-04-11 11:49:22 -0700

    Moved the entry container styled thing into the Entry file

--- src/main-view/Entry.js
+++ src/main-view/Entry.js
@@ -1,10 +1,30 @@
 import React from "react";
-import EntryContainer from "./EntryContainer";
+// import EntryContainer from "./EntryContainer";
 import EntryContents from "./EntryContents";
 import EntryDate from "./EntryDate";
 import EntryGrade from "./EntryGrade";
 import { addCaptionToImgFromAltText } from "../utils/image-caption-script";
+/** @jsx jsx */
+import { css, jsx } from "@emotion/core";
 // import { globals } from "../globals";
+
+const EntryContainer = props => (
+  <div
+    css={css`
+      position: relative; /* Must be included for absolutely positioned children */
+      display: flex;
+      flex-direction: row;
+      flex-wrap: nowrap;
+      width: 90%;
+      min-width: 450px;
+      height: auto;
+      margin: 0px 0 100px 0;
+      padding: 0 5% 0 5%;
+    `}
+  >
+    {props.children}
+  </div>
+);
 
 class Entry extends React.Component {
   constructor(props) {
