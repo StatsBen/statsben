fc060e0f44a52f6d359c64ee9662ed3df5536640
Ben Clark <bclark@avalancheassociation.ca>
2020-09-03 10:22:58 -0700

    Updated yet another bug in the date component

--- src/Entry.js
+++ src/Entry.js
@@ -33,7 +33,7 @@ class Entry extends React.Component {
     const date = new Date(entry.date.toDate());
     const rawDay = date.getDate();
     const day = rawDay < 10 ? "0" + rawDay : rawDay;
-    const rawMonth = date.getMonth();
+    const rawMonth = date.getMonth() + 1;
     const month = rawMonth < 10 ? "0" + rawMonth : rawMonth;
     const dateStr = day + "-" + month + "-" + date.getFullYear();
     return dateStr;
