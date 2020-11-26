f11b529dcfd256760ccf05c141605226005aa90f
Ben Clark <bclark@avalancheassociation.ca>
2020-11-26 15:56:03 -0800

    Added a little scroll-reset kind of feature thing...

--- src/main-view/desktop-viewer/DesktopNavigation.js
+++ src/main-view/desktop-viewer/DesktopNavigation.js
@@ -8,6 +8,7 @@ import {
 } from "./DesktopStyledComponents";
 
 const NAV_MAX_WIDTH = "280px";
+const ENTRY_SCROLL_TOP = 605;
 
 const DesktopNavigation = props => {
   const {
@@ -23,19 +24,42 @@ const DesktopNavigation = props => {
     entries.length
   } ) `;
 
+  const handleNextAndResetScroll = () => {
+    handleNextClick();
+    resetScrollY();
+  };
+
+  const handlePrevAndResetScroll = () => {
+    handlePrevClick();
+    resetScrollY();
+  };
+
+  const handleTileClick = entry => {
+    setActiveEntry(entry);
+    resetScrollY();
+  };
+
+  const resetScrollY = () => {
+    const currentScroll = window.scrollY;
+    if (currentScroll > ENTRY_SCROLL_TOP)
+      setTimeout(() => {
+        window.scrollTo({ top: ENTRY_SCROLL_TOP, behavior: "smooth" });
+      }, 200);
+  };
+
   return (
-    <NavContainer maxWidth={NAV_MAX_WIDTH} >
+    <NavContainer maxWidth={NAV_MAX_WIDTH}>
       <CarouselContainer maxWidth={NAV_MAX_WIDTH}>
-        <Butt onClick={handlePrevClick}>prev</Butt>
+        <Butt onClick={handlePrevAndResetScroll}>prev</Butt>
         <i>{paginationString}</i>
-        <Butt onClick={handleNextClick}>next</Butt>
+        <Butt onClick={handleNextAndResetScroll}>next</Butt>
       </CarouselContainer>
       <Tiles maxWidth={NAV_MAX_WIDTH} height={height}>
         {entries.map((entry, i) => (
           <EntryTile
             key={`e-tile-${i}`}
             entry={entry}
-            clickHandler={() => setActiveEntry(entry)}
+            clickHandler={() => handleTileClick(entry)}
             isActive={entry === activeEntry}
           />
         ))}
